016e9a6e5c7f1ebb6fe36f0d5696de4bb0f9accb
Yongchan Kwon <ykwon.0407@gmail.com>
2016-01-21 12:07:20 +0900

    codility2_2

---
+++ codility/codility2_2.py
@@ -0,0 +1,18 @@
+# you can write to stdout for debugging purposes, e.g.
+# print "this is a debug message"
+
+def solution(A, K):
+    # write your code in Python 2.7
+    if K is 0 or A == []:
+        return A
+    else:
+        N = len(A)
+        B = [0]*N
+        B[0] = A[-1]
+        B[1:N] = A[0:(N-1)]
+        return solution(B,K-1)
+
+
+A=[]
+K=3
+print solution(A,K)
